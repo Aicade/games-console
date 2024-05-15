2571fb06b802d83c9ff9c90bc88d91b7be8983a0
A.S.Rawat <adityas@braincade.in>
2024-05-15 19:07:06 +0530

    update

--- public/games/Go-around.js
+++ public/games/Go-around.js
@@ -6,8 +6,15 @@ let assetsLoader = {
 
 let soundsLoader = {
     "background": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/music/bgm-3.mp3",
-    "lose": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/lose_2.mp3"
+    "lose": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/lose_2.mp3",
+    "jump": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/jump_1.mp3",
+    "spawn": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/jump_2.mp3",
 }
+
+// this.load.audio('flap', ['https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/jump_3.mp3']);
+// this.load.audio('collect', ['https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/collect_1.mp3']);
+// this.load.audio('lose', ['https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/lose_2.mp3']);
+// this.load.audio('damage', ['https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/jump_2.mp3']);
 
 const orientationSizes = {
     "landscape": {
@@ -65,8 +72,6 @@ class GameScene extends Phaser.Scene {
 
         this.load.image("pauseButton", "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/pause.png");
         this.load.image("pillar", "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/textures/Bricks/s2+Brick+01+Grey.png");
-        this.load.audio('bgm', ['https://aicade-ui-assets.s3.amazonaws.com/GameAssets/music/bgm-3.mp3']);
-        this.load.audio('lose', ['https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/lose_2.mp3']);
 
         const fontName = 'pix';
         const fontBaseURL = "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/"
@@ -156,13 +161,6 @@ class GameScene extends Phaser.Scene {
 
         this.player.setBodySize(newWidth, newHeight, true);
 
-
-        // this.isJumping = false;
-        this.jumpCount = 0;
-        this.maxJump = 2; // Allow up to 2 jumps (double jump)
-
-
-        // Handle input
         this.input.on('pointerdown', this.jump, this);
         this.spawnEnemyTimer = this.time.addEvent({
             delay: this.enemySpawnDelay,
@@ -186,15 +184,15 @@ class GameScene extends Phaser.Scene {
     }
 
     levelUp() {
-        this.levelUpText.setAlpha(1);  // Make the text fully visible before starting to blink
+        this.levelUpText.setAlpha(1);
 
         this.tweens.add({
             targets: this.levelUpText,
-            alpha: 0,  // Tween the alpha to 0
-            ease: 'Linear',  // Consistent blinking rate
-            duration: 500,  // Duration to fade out
-            repeat: 1,  // Calculate number of repeats to achieve approximately 500 seconds
-            yoyo: false  // Return to visible after reaching 0
+            alpha: 0,
+            ease: 'Linear',
+            duration: 500,
+            repeat: 1,
+            yoyo: false
         });
     }
 
@@ -232,7 +230,6 @@ class GameScene extends Phaser.Scene {
         const playerX = this.circle.x + Math.cos(this.playerAngle) * this.playerDistance;
         const playerY = this.circle.y + Math.sin(this.playerAngle) * this.playerDistance;
         this.player.setPosition(playerX, playerY);
-        // this.player.rotation = -this.playerAngle;
         this.player.rotation = this.playerAngle - Math.PI - 30;
 
     }
@@ -241,6 +238,7 @@ class GameScene extends Phaser.Scene {
         const jumpDistance = 200;
 
         if (this.playerDistance == 255) {
+            this.sounds.jump.setVolume(1).setLoop(false).play();
             this.tweens.add({
                 targets: this,
                 playerDistance: this.circle.radius - jumpDistance,
@@ -250,6 +248,9 @@ class GameScene extends Phaser.Scene {
                 onUpdate: () => {
                     this.player.x = this.circle.x + Math.cos(this.playerAngle) * this.playerDistance;
                     this.player.y = this.circle.y + Math.sin(this.playerAngle) * this.playerDistance;
+                },
+                onComplete: () => {
+                    this.vfx.shakeCamera(200);
                 }
             });
         }
@@ -257,22 +258,21 @@ class GameScene extends Phaser.Scene {
 
     spawnEnemy() {
         if (!this.isGameOver) {
-            // Generate a random number between 0.7 and 1.3
             const randomAngle = Phaser.Math.FloatBetween(0.7, 1.3);
-
-            // console.log(randomAngle);
 
             const spawnAngle = this.playerAngle + randomAngle;
             const x = this.circle.x + Math.cos(spawnAngle) * this.circle.radius;
             const y = this.circle.y + Math.sin(spawnAngle) * this.circle.radius;
             const enemy = this.enemies.create(x, y, 'enemy').setScale(.05);
-            const newWidth = enemy.body.width * 0.5; // Reduce width by 20%
+            const newWidth = enemy.body.width * 0.6; // Reduce width by 20%
             const newHeight = enemy.body.height * 0.8; // Reduce height by 20%
 
             enemy.body.setSize(newWidth, newHeight);
 
             this.vfx.addShine(enemy, 4000, .3);
             this.vfx.rotateGameObject(enemy);
+            this.sounds.spawn.setVolume(.75).setLoop(false).play();
+
             this.time.delayedCall(5000, () => enemy.destroy(), [], this);
         }
     }
