e2662b72301167dd3031b9622058ba80dbf4ea0b
Ankur <ankbiz32@gmail.com>
2024-05-25 16:29:34 +0530

    Mario bros debug false

--- public/games/Mariobros.js
+++ public/games/Mariobros.js
@@ -710,7 +710,7 @@ const config = {
         default: "arcade",
         arcade: {
             gravity: { y: 300 },
-            debug: true,
+            debug: false,
         },
     },
     dataObject: {
