4ebf16ebff0efa8b1fd3016b6120397bb9bd40c3
Ankur <ankbiz32@gmail.com>
2024-07-01 23:51:42 +0530

    SaveEarth game added

--- public/games/saveEarth.js
+++ public/games/saveEarth.js
@@ -7,18 +7,19 @@ let assetsLoader = {
 let soundsLoader = {
     "background": "background",
     "success": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/upgrade_1.mp3",
+    "shoot": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/blast.mp3",
     "damage": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/damage_1.mp3",
     "loose": "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/sfx/lose_2.mp3"
 };
 
 // Custom UI Elements
-const title = `CORONA DODGE`
-const description = `Control game. 
-Let's see how good are your controls! Move and dodge the enemeis 
+const title = `Save Me`
+const description = `Reaction game. 
+Let's see how good is your reaction! Tap & destroy objects
 coming from all direction.`
 const instructions =
     `Instructions:
-    1. Use arrow keys or touch to avoid the enemies.`;
+    1. Tap and destroy objects.`;
 
 const orientationSizes = {
     "landscape": {
@@ -54,13 +55,12 @@ class GameScene extends Phaser.Scene {
             this.load.audio(key, [soundsLoader[key]]);
         }
 
-        this.load.image('heart', 'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/heart.png');
         this.load.image("pauseButton", "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/pause.png");
         this.load.bitmapFont('pixelfont',
             'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.png',
             'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.xml');
         addEventListenersPhaser.bind(this)();
-        
+
         displayProgressLoader.call(this)
     }
 
@@ -70,21 +70,14 @@ class GameScene extends Phaser.Scene {
         this.height = this.game.config.height;
         this.score = 0;
         this.level = 0;
-        this.playerSpeed = 400;
-        this.playerSpeedOnTouch = this.playerSpeed * 2;
 
-        this.maxEnemySpeed = 1000;
-        this.enemySpeed = 100;
-        this.enemySpeedInc = 40;
+        this.maxEnemySpeed = 1500;
+        this.enemySpeed = 450;
+        this.enemySpeedInc = 80;
 
         this.maxEnemies = 15;
-        this.minSpawnerInterval = 100;
-        this.enemySpawnTimeInteval = 500;
-
-        this.pointerDown = false;
-        this.px = 0;
-        this.py = 0;
-
+        this.minSpawnerInterval = 500;
+        this.enemySpawnTimeInteval = 1000;
         this.level = 0;
         this.levelScoreThreshold = 100;
 
@@ -104,13 +97,6 @@ class GameScene extends Phaser.Scene {
         this.levelText = this.add.bitmapText(20, 20, 'pixelfont', 'Level: 0', 28).setDepth(11);
         this.levelUpText = this.add.bitmapText(this.width / 2, 100, 'pixelfont', 'LEVEL UP', 50).setOrigin(0.5, 0.5).setAlpha(0).setDepth(11);
 
-        this.lives = 3;
-        this.hearts = [];
-        for (let i = 0; i < this.lives; i++) {
-            let x = 40 + (i * 35);
-            this.hearts[i] = this.add.image(x, 100, "heart").setScale(0.025).setDepth(11);
-        }
-
         // Add input listeners
         this.input.keyboard.on('keydown-ESC', () => this.pauseGame());
         this.pauseButton = this.add.image(this.game.config.width - 60, 60, "pauseButton");
@@ -118,63 +104,32 @@ class GameScene extends Phaser.Scene {
         this.pauseButton.setScale(2).setScrollFactor(0).setDepth(11);
         this.pauseButton.on('pointerdown', () => this.pauseGame());
 
-        this.cursor = this.input.keyboard.createCursorKeys();
-
-        this.player = this.physics.add.sprite(this.game.config.width / 2, this.game.config.height / 2, 'player');
+        this.player = this.physics.add.sprite(this.width / 2, this.height / 2, 'player');
+        this.player.health = 100;
         this.player.setScale(0.15).setDepth(11);
         this.player.body.setSize(this.player.body.width / 1.5, this.player.body.height / 1.5);
-        this.player.setCollideWorldBounds(true);
+        this.player.setCollideWorldBounds(true).setImmovable(true);
+        this.player.setAngularVelocity(20);
+
+        this.makeHealthMeter();
 
         this.particleEmitter = this.vfx.createEmitter('avoidable', 0, 0, 0.02, 0, 600).setAlpha(0.5);
-        this.whiteParticle = this.vfx.addCircleTexture('whiteCircle', 0xffffff, 1, 8);
-        this.followEmitter = this.vfx.createEmitter('whiteCircle', 0, 0, 1, 0, 600).setAlpha(0.8);
-        this.followEmitter.startFollow(this.player);
         this.playerParticleEmitter = this.vfx.createEmitter('player', 0, 0, 0.03, 0, 1000).setAlpha(0.5);
 
-        this.enemies1 = this.physics.add.group();
+        this.enemies = this.physics.add.group();
 
         this.enemyTimer = this.time.addEvent({ delay: this.enemySpawnTimeInteval, callback: this.spawnEnemy, callbackScope: this, loop: true });
-        this.time.addEvent({ delay: 1000, callback: this.incrementScoreAndLevel, callbackScope: this, loop: true });
 
-        // Enable touch input for moving the player
-        this.input.on('pointerdown', () => {
-            this.pointerDown = true;
+        this.physics.add.collider(this.player, this.enemies, (player, enemy) => {
+            enemy.destroy();
+            this.playerHit(player, enemy);
         });
 
-        // Stop the player when the touch is released
-        this.input.on('pointerup', () => {
-            this.pointerDown = false;
-        });
         this.input.keyboard.disableGlobalCapture();
 
     }
 
-    update(time, delta) {
-        if (this.gameOverFlag) return;
-        this.player.setVelocity(0);
-
-        this.followEmitter.stop();
-        if (this.cursor.left.isDown) {
-            this.followEmitter.start();
-            this.player.setVelocityX(-this.playerSpeed);
-        } else if (this.cursor.right.isDown) {
-            this.followEmitter.start();
-            this.player.setVelocityX(this.playerSpeed);
-        }
-
-        if (this.cursor.up.isDown) {
-            this.followEmitter.start();
-            this.player.setVelocityY(-this.playerSpeed);
-        } else if (this.cursor.down.isDown) {
-            this.followEmitter.start();
-            this.player.setVelocityY(this.playerSpeed);
-        }
-
-
-        if (this.pointerDown && !this.player.body.hitTest(this.input.x, this.input.y)) {
-            this.physics.moveTo(this.player, this.input.x, this.input.y, this.playerSpeedOnTouch);
-        }
-        this.physics.world.collide(this.player, this.enemies1, this.playerHit, null, this);
+    update() {
 
     }
 
@@ -195,12 +150,10 @@ class GameScene extends Phaser.Scene {
         this.level = Math.floor(this.score / this.levelScoreThreshold);
         this.updateLevel(this.level);
         if (oldLevel < this.level) {
-
-            if (this.gameOverFlag) return;
             this.vfx.blinkEffect(this.levelUpText, 400, 3);
             this.sounds.success.setVolume(0.7).setLoop(false).play()
             if (this.enemySpawnTimeInteval > this.minSpawnerInterval && !(this.level % 3)) {
-                this.enemySpawnTimeInteval -= 20;
+                this.enemySpawnTimeInteval -= 40;
             }
             if (this.enemySpeed < this.maxEnemySpeed) {
                 this.enemySpeed += this.enemySpeedInc;
@@ -214,11 +167,11 @@ class GameScene extends Phaser.Scene {
     }
 
     spawnEnemy() {
-        if (this.enemies1.getChildren().length > this.maxEnemies) {
-            this.enemies1.remove(this.enemies1.getChildren()[0], true, true);
+        if (this.enemies.getChildren().length > this.maxEnemies) {
+            this.enemies.remove(this.enemies.getChildren()[0], true, true);
         }
         let enemy;
-        let x, y, tox, toy;
+        let x, y;
         let randu = Phaser.Math.Between(0, 3);
 
         if (randu === 0) {
@@ -234,57 +187,65 @@ class GameScene extends Phaser.Scene {
             x = this.game.config.width;
             y = Phaser.Math.Between(0, this.game.config.height - 20);
         }
+        enemy = this.enemies.create(x, y, 'avoidable');
 
-        tox = Phaser.Math.Between(0, this.game.config.width);
-        toy = Phaser.Math.Between(0, this.game.config.height);
-
-        enemy = this.enemies1.create(x, y, 'avoidable');
         enemy.setScale(0.07);
+
+        enemy.flamesParticle = this.vfx.addCircleTexture('flames', 0xffa500, 1, 6);
+        enemy.followEmitter = this.vfx.createEmitter('flames', 0, 0, 1, 0, 500).setAlpha(0.8);
+        enemy.followEmitter.startFollow(enemy).start();
+        enemy.setDepth(11);
+        enemy.setAngularVelocity((Math.random() < 0.5) ? -200 : 200)
+
         enemy.body.setSize(enemy.body.width / 1.5, enemy.body.height / 1.5);
-        this.physics.moveTo(enemy, tox, toy, this.enemySpeed);
-        (Math.random() < 0.5) ? -50 : 50
-        enemy.setAngularVelocity((Math.random() < 0.5) ? -80 : 80)
-        this.tweens.add({
-            targets: enemy,
-            scale: enemy.scale + 0.02,
-            duration: 600,
-            yoyo: true,
-            loop: -1,
-            ease: 'sine.inout'
+        this.physics.moveTo(enemy, this.player.x, this.player.y, this.enemySpeed);
+        enemy.setInteractive({ cursor: "pointer" })
+        enemy.on('pointerdown', () => {
+            this.sounds.shoot.play();
+            this.particleEmitter.explode(100, enemy.x, enemy.y);
+            this.incrementScoreAndLevel();
+            enemy.destroy();
+            enemy.followEmitter.stop().destroy();
         });
 
     }
 
-     playerHit(player, enemy) {
-        this.lives--;
-        if (this.lives > 0) {
-            this.damageMusic.play();
-            this.particleEmitter.explode(100, enemy.x, enemy.y)
-            enemy.destroy();
-            this.hearts[this.lives].destroy();
+    playerHit(player, enemy) {
+        this.player.health -= 10;
+        
+        if (this.player.health <= 30) {
+            this.healthMeter.fillColor = 0xff0000;
+        }
+        this.healthMeter.width = this.healthMeter.width - 15;
+        
+        enemy.destroy();
+        enemy.followEmitter.stop().destroy();
+
+        if (this.player.health > 0) {
+            this.sounds.damage.play();
+            this.particleEmitter.explode(100, enemy.x, enemy.y);
             this.vfx.shakeCamera(200, 0.01);
         } else {
-            this.gameOverFlag = true;
-            this.hearts[this.lives].destroy();
-            this.followEmitter.stop();
+            this.enemyTimer.destroy();
             this.physics.pause();
             this.sound.stopAll();
             this.player.setTint(0xff0000);
 
             this.time.delayedCall(1000, () => {
-
                 this.playerParticleEmitter.explode(400, player.x, player.y);
-                this.looseMusic.play();
+                this.sounds.loose.play();
                 this.vfx.shakeCamera(300, 0.04);
                 this.player.destroy();
                 this.time.delayedCall(2000, () => {
                     this.gameOver();
                 });
-
             });
-
         }
-        this.vfx.rotateGameObject(this.player, 400)
+    }
+
+    makeHealthMeter(x = this.width/2, y = this.player.y - 100, width = 150, height = 15) {
+        this.healthMeterBg = this.add.rectangle(x, y, width, height, 0x888888);
+        this.healthMeter = this.add.rectangle(x, y, width, height, 0x00ff00);
     }
 
     updateScoreText() {
@@ -335,7 +296,7 @@ function displayProgressLoader() {
         progressBar.fillRect(x, y, width * value, height);
     });
     this.load.on('fileprogress', function (file) {
-         
+
     });
     this.load.on('complete', function () {
         progressBar.destroy();
