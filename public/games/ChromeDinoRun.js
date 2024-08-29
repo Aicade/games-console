e45fda32120894fafbfae6e0cada62abcece2f78
Atul Singh <atul@braincade.in>
2024-08-29 20:43:48 +0530

    ChromeDino new

--- public/games/ChromeDinoRun.js
+++ public/games/ChromeDinoRun.js
@@ -312,5 +312,5 @@ const config = {
         description: _CONFIG.description,
         instructions: _CONFIG.instructions,
     },
-    deviceOrientation: _CONFIG.deviceOrientation,
+    deviceOrientation: _CONFIG.deviceOrientation==="landscape"
 };
